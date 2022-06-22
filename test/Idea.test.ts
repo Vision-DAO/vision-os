0672f9faefb87a8860b85747cd8e1725b351f0ae
Dowland Aiello <dowlandaiello@gmail.com>
2022-06-22 18:55:21 -0400

    test: Start building out tests for contracts and schemas.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- test/Idea.test.ts
+++ test/Idea.test.ts
@@ -1,17 +1,98 @@
 import { ethers, waffle } from "hardhat";
-import chai from "chai";
+import { expect } from "chai";
 import { describe } from "mocha";
 
 import IdeaContract from "../artifacts/contracts/Idea.sol/Idea.json";
 import { Idea } from "../types/contracts/Idea";
+import { IPFSClient, SCHEMA, MODULES, forAllModules, bytesEqual } from "./common";
+
+import { create as createValidator } from "ipld-schema-validator";
+import { parse as parseSchema, Schema } from "ipld-schema";
+import { create } from "ipfs";
+import fs from "fs";
+
+const { loadFixture } = waffle;
+
+/**
+ * Deploys an instance of the beacon layer idea metadata schema with the given
+ * details, returning the CID of the deployed instance.
+ */
+const createIdeaMetadata = async (ipfs: IPFSClient, title: string, description: string, payload: Uint8Array): Promise<string> => {
+	return await ipfs.
+};
 
 /**
  * Tests the functionality of the Idea constructor contract, its ERC20
  * properties, the IPLD schema, and not much else. Governance tests are in
  * Prop.test.ts.
+ *
+ * HELP: These tests rely on two packages made by protocol labs that we use for
+ * validating that data with a shape our solidity and typescript code expects
+ * conforms with the schema declared in the fixtures folder. You can see some
+ * helpful examples and documentation
+ * here: https://github.com/ipld/js-ipld-schema
  */
 describe("Idea", () => {
-	it("Should be constructable from an instance of the schema", async () => {
+	const fixture = async (): Promise<{ ipfs: IPFSClient, schema: Schema }> => {
+		// Make sure not to save state between any IPFS instances by using
+		// multiple, disjointed repos
+		return { ipfs: await create({ repo: `ipfs_${Math.random() * 100}` }), schema: parseSchema(SCHEMA) };
+	};
 
+	describe("IdeaPayload schema", () => {
+		it("Should be uploadable from a Uint8Array", async () => {
+			const { ipfs, schema } = await loadFixture(fixture);
+			const validator = createValidator(schema, "IdeaPayload");
+
+			// Make sure each module can be treated as a schema instance
+			forAllModules(async (mod) => {
+				expect(validator(mod)).to.be.true;
+				expect(await ipfs.dag.put(mod)).to.not.be.empty;
+			});
+		});
+
+		it("Should be equivalent in locally stored and remote form", async () => {
+			const { ipfs, schema } = await loadFixture(fixture);
+			const validator = createValidator(schema, "IdeaPayload");
+
+			// Make sure the local disk and the remote version of the module are
+			// exactly the same
+			forAllModules(async (mod) => {
+				const cid = await ipfs.dag.put(mod);
+				const retrieved = await ipfs.dag.get(cid);
+
+				expect(validator(retrieved)).to.be.true;
+				expect(bytesEqual(mod, retrieved)).to.be.true;
+			});
+	});
+
+	describe("IdeaMetadata schema", () => {
+		it("Should be validly constructable from a JavaScript object", async () => {
+			const { ipfs, schema } = await loadFixture(fixture);
+
+			const validator = createValidator(schema, "IdeaMetadata");
+
+			// Test making a metadata object for every available WASM module
+			forAllModules(async (mod) => {
+				const modCid = await ipfs.dag.put(mod);
+
+				const exampleMetadata = {
+					title: "Example DAO",
+					description: "An example.",
+					payload: modCid,
+				};
+
+				// Ensure that the schema does not conflict with our usage
+				expect(validator(exampleMetadata)).to.be.true;
+			});
+		});
+
+		it("Should be uploadable to the IPFS network", async () => {
+
+		});
+	});
+
+	it("Should be constructable from an instance of the schema", async () => {
+		const ipfs = await loadFixture(fixture);
 	});
 });
---
+++ test/common.ts
@@ -0,0 +1,65 @@
+import fs from "fs";
+import { create } from "ipfs";
+
+/**
+ * An instance of IPFS used for harnessing a test.
+ */
+export type IPFSClient = ReturnType<Awaited<typeof create>>;
+
+/**
+ * The path to the file containing an IPLD schema to parse describing Vision
+ * metadata.
+ */
+export const SCHEMA_PATH: string = process.env.SCHEMA_PATH || "../fixtures/beacon_layer.ipldsch";
+
+/**
+ * The path to a directory of .wasm files that should be loaded for testing
+ * purposes. No extensive testing of these modules' functionality is performed.
+ */
+export const MODULES_PATH: string = process.env.MODULES_PATH || "../fixtures/modules/target/wasm32-unknown-unknown/release/";
+
+/**
+ * wasm-pack will stubbornly compile the root src/lib.rs for the modules
+ * fixture, but we don't want to actually run it.
+ */
+export const MODULE_IGNORES: string[] = (process.env.MODULE_IGNORES || "beacon_dao_modules").split(" ");
+
+/**
+ * The paths to every WASM module that can be used by the testing suite.
+ */
+export const MODULES: string[] = fs
+	.readdirSync(MODULES_PATH)
+	.filter((path) =>
+		!(path.substring(path.lastIndexOf(".")) in MODULE_IGNORES)
+	);
+
+/**
+ * The IPLD schema used for validating compatibility with test methodology.
+ */
+export const SCHEMA: string = fs.readFileSync(SCHEMA_PATH, "utf-8");
+
+/**
+ * Executes a test for every WASM module loaded for testing.
+ */
+export const forAllModules = async (fn: (module: Uint8Array) => Promise<void>) => {
+	for (const modPath of MODULES) {
+		const modContents = fs.readFileSync(modPath);
+
+		await fn(modContents);
+	}
+}
+
+/**
+ * Returns true if the two uint8arrays are equivalent, or false if they are not.
+ */
+export const bytesEqual = (a: Uint8Array, b: Uint8Array): boolean => {
+	if (a.length != b.length)
+		return false;
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] != b[i])
+			return false;
+	}
+
+	return true;
+};
