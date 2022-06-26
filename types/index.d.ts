ff10124547a03126ce98ba373b8ab2545f86f358
Dowland Aiello <dowlandaiello@gmail.com>
2022-06-26 12:39:03 -0400

    Make type declarations accessible to consuming packages.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+export type { openzeppelin, contracts, Ownable, ERC20, IERC20Metadata, IERC20, Idea, Proposal } from "./index";
+export type { IdeaPayload, IdeaMetadata, ProposalPayload, ProposalMetadata } from "./schema";
