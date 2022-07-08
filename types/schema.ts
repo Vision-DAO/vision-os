ce7dfd9341eff4e4e57598d5a1ac78477b92a25d
Dowland Aiello <dowlandaiello@gmail.com>
2022-07-07 22:31:34 -0400

    schema: Add convenience types describing the shape of a WASM module.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- types/schema.ts
+++ types/schema.ts
@@ -1,6 +1,21 @@
 import { CID } from "multiformats/cid";
 
 /**
+ * A basic payload for the Beacon DAO that specifies a void entrypoint, start.
+ */
+export interface Payload {
+	start(): void
+}
+
+/**
+ * A JavaScript loader that consumes a WASM module and produces a JS wrapper
+ * that can call the entrypoint method.
+ */
+export interface PayloadLoader {
+	init(module: Uint8Array): Payload,
+}
+
+/**
  * Binary representation of a WASM program.
  */
 export interface IdeaPayload {
