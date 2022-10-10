72c1cbe525a7cf1751ed64010f53cafae77b4fc3
Dowland Aiello <dowlandaiello@gmail.com>
2022-10-10 15:42:28 -0700

    Start implementing read.

--- fixtures/modules/beacon_dao-allocator/src/lib.rs
+++ fixtures/modules/beacon_dao-allocator/src/lib.rs
@@ -19,6 +19,9 @@ macro_rules! eassert {
 /// owner is specified.
 static mut OWNER: Option<Address> = None;
 
+/// The contents of the memory cell.
+static mut VAL: Vec<u8> = Vec::new();
+
 struct InitMessage {}
 
 #[wasm_bindgen]
@@ -29,4 +32,26 @@ pub fn handle_allocate(from: Address, size: u32) {
 	let memcell = spawn_actor(address());
 
 	send_message(memcell, "")
+}
+
+#[wasm_bindgen]
+pub fn init(owner: Address) {
+	OWNER.replace(Some(owner));
+}
+
+#[wasm_bindgen]
+pub fn handle_read(from: Address, offset: u32) {
+	if let Some(val) = VAL.get(offset) {
+		send_message(from, "read_ok", val);
+	} else {
+		let err = spawn_actor(address());
+		send_message(from, "read_err", 
+	}
+}
+
+/// Initializes the memory cell from a string.
+macro_rules! from_str {
+	($s:expr) => {
+		let cell = send_message(
+	}
 }
