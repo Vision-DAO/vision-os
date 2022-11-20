05c8c7c417d156fa75c1e5d033676ec200549f00
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-19 18:47:14 -0800

    Implement proxy switching for new allocator manager.

--- fixtures/modules/beacon_dao-allocator_manager/src/lib.rs
+++ fixtures/modules/beacon_dao-allocator_manager/src/lib.rs
@@ -1,8 +1,13 @@
 use beacon_dao_allocator::{allocate, read, write};
-use vision_utils::types::Callback;
+use beacon_dao_permissions::{has_permission, register_permission};
+use vision_utils::types::{Callback, ALLOCATOR_IMPL_ADDR, PERM_ADDR};
+
+const PERM: &'static str = "change_proxy_allocator";
+const DESCRIPTION: &'static str =
+	"Allows the app to change which app your Vision OS uses for allocation.";
 
 /// The address of the actor implementing the API.
-static PROXY: RwLock<Option<Address>> = RwLock::new(None);
+static PROXY: RwLock<Option<Address>> = RwLock::new(Some(ALLOCATOR_IMPL_ADDR));
 
 macro_rules! with_proxy {
 	() => {
@@ -14,7 +19,38 @@ macro_rules! with_proxy {
 	};
 }
 
+#[cfg(feature = "module")]
+#[no_mangle]
+pub fn init(owner: Address) {
+	register_permission(PERM.into_owned(), DESCRIPTION.into_owned());
+}
+
 /// Bare API methods, which are proxied to PROXY, if it exists
+
+#[no_mangle]
+#[with_bindings]
+pub extern "C" fn handle_change_proxy(from: Address, proxy: Address, callback: Callback<u8>) {
+	has_permission(
+		PERM_ADDR,
+		from,
+		"change_proxy_allocator".into(),
+		Callback::new(|has_perm| {
+			if !has_perm {
+				callback(1);
+
+				return;
+			}
+
+			if let Some(ref mut proxy) = PROXY.write() {
+				proxy.replace(Some(proxy));
+
+				callback(0);
+			} else {
+				callback(1);
+			}
+		}),
+	);
+}
 
 #[no_mangle]
 #[with_bindings]
--- fixtures/modules/beacon_dao-permissions/src/lib.rs
+++ fixtures/modules/beacon_dao-permissions/src/lib.rs
@@ -1,7 +1,5 @@
 use vision_derive::with_bindings;
-/*
-TODO: Requires window manager to work.
-use vision_utils::types::Address;
+use vision_utils::types::{Address, Callback};
 
 use std::collections::{HashMap, HashSet};
 
@@ -9,26 +7,46 @@ use std::collections::{HashMap, HashSet};
 static PERMISSIONS: Arc<RwLock<HashMap<String, (String, HashSet<Address>)>>>;
 
 /// Registers a capability of the Vision OS that the user needs to consent to allowing.
+#[no_mangle]
 #[with_bindings]
-pub fn handle_register_permission(from: Address, name: String, description: String) {
-	PERMISSIONS
-		.write()
-		.unwrap()
-		.entry(name)
-		.or_insert((description, HashSet::new()));
+pub fn handle_register_permission(
+	from: Address,
+	name: String,
+	description: String,
+	callback: Callback<u8>,
+) {
+	if let Some(lock) = PERMISSIONS.write() {
+		lock.entry(name).or_insert((description, HashSet::new()));
+
+		callback(0);
+	} else {
+		callback(1);
+	};
 }
 
 /// Checks that the actor has the identified permission.
+#[no_mangle]
 #[with_bindings]
-pub fn handle_has_permission(from: Address, actor: Address, permission: String) -> bool {
-	PERMISSIONS
-		.read()
-		.unwrap()
-		.get(permission)
-		.and_then(|actors_with_perm| actors_with_perm.has(actor))
-		.unwrap_or(false)
+pub fn handle_has_permission(
+	from: Address,
+	actor: Address,
+	permission: String,
+	callback: Callback<bool>,
+) {
+	if let Some(lock) = PERMISSIONS.read() {
+		callback(
+			lock.get(permission)
+				.and_then(|actors_with_perm| actors_with_perm.has(actor))
+				.unwrap_or(false),
+		);
+	} else {
+		callback(false);
+	}
 }
 
+#[no_mangle]
 #[with_bindings]
-pub fn handle_request_permission(from: Address, permission: String) -> bool {}
-*/
+pub fn handle_request_permission(from: Address, permission: String, callback: Callback<bool>) {
+	// TODO: Implement this
+	unimplemented!()
+}
