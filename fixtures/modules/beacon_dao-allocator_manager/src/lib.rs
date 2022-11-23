e67ac5c276ca79fb77b1e65082032493b259612c
Dowland Aiello <dowlandaiello@gmail.com>
2022-11-23 09:49:25 -0800

    Use beacon_dao_allocator from vision_derive inside manager.

--- fixtures/modules/beacon_dao-allocator_manager/src/lib.rs
+++ fixtures/modules/beacon_dao-allocator_manager/src/lib.rs
@@ -1,6 +1,8 @@
-use beacon_dao_allocator::{allocate, read, write};
 use beacon_dao_permissions::{has_permission, register_permission};
-use vision_utils::types::{Callback, ALLOCATOR_IMPL_ADDR, PERM_ADDR};
+use vision_derive::{beacon_dao_allocator, with_bindings};
+use vision_utils::types::{Address, Callback, ALLOCATOR_IMPL_ADDR, PERM_ADDR};
+
+use std::{ops::DerefMut, sync::RwLock};
 
 const PERM: &'static str = "change_proxy_allocator";
 const DESCRIPTION: &'static str =
@@ -11,18 +13,23 @@ static PROXY: RwLock<Option<Address>> = RwLock::new(Some(ALLOCATOR_IMPL_ADDR));
 
 macro_rules! with_proxy {
 	() => {
-		if let Some(proxy) = PROXY.read() {
+		if let Ok(Some(proxy)) = PROXY.read().map(|cts| *cts) {
 			proxy
 		} else {
 			return;
-		};
+		}
 	};
 }
 
 #[cfg(feature = "module")]
 #[no_mangle]
 pub fn init(owner: Address) {
-	register_permission(PERM.into_owned(), DESCRIPTION.into_owned());
+	register_permission(
+		PERM_ADDR,
+		PERM.to_owned(),
+		DESCRIPTION.to_owned(),
+		Callback::new(|_| {}),
+	);
 }
 
 /// Bare API methods, which are proxied to PROXY, if it exists
@@ -34,19 +41,19 @@ pub extern "C" fn handle_change_proxy(from: Address, proxy: Address, callback: C
 		PERM_ADDR,
 		from,
 		"change_proxy_allocator".into(),
-		Callback::new(|has_perm| {
+		Callback::new(move |has_perm: bool| {
 			if !has_perm {
-				callback(1);
+				callback.call(1);
 
 				return;
 			}
 
-			if let Some(ref mut proxy) = PROXY.write() {
-				proxy.replace(Some(proxy));
+			if let Ok(ref mut proxy_buf) = PROXY.write() {
+				proxy_buf.replace(proxy);
 
-				callback(0);
+				callback.call(0);
 			} else {
-				callback(1);
+				callback.call(1);
 			}
 		}),
 	);
@@ -57,5 +64,5 @@ pub extern "C" fn handle_change_proxy(from: Address, proxy: Address, callback: C
 pub extern "C" fn handle_allocate(from: Address, size: u32, callback: Callback<Address>) {
 	let proxy = with_proxy!();
 
-	allocate(proxy, from, size, Callback::new(|addr| callback(addr)));
+	beacon_dao_allocator::allocate(proxy, from, size, Callback::new(|addr| callback.call(addr)));
 }
